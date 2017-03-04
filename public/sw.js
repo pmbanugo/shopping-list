7f461a8f79b13a7f88949365a7dae3e5af669b8d
Peter Mbanugo <chuksgerrard@yahoo.com>
2017-03-04 16:27:35 +0100

    updated service worker script

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 importScripts("/resources/sw-toolbox.js");
 
-const version = "0.12";
+const version = "0.2";
 
 self.addEventListener("install", function(event){
     event.waitUntil(
@@ -17,6 +17,7 @@ self.addEventListener("install", function(event){
                         "/css/style.css",
                         "/resources/dialog-polyfill/dialog-polyfill.js",
                         "/resources/dialog-polyfill/dialog-polyfill.css",
+                        "/resources/system.js",
                         "/js/transpiled/index.js",
                         "/js/transpiled/history.js",
                         "/js/transpiled/shared.js",
@@ -32,10 +33,7 @@ self.addEventListener("install", function(event){
 });
 
 self.addEventListener("activate", function (event){
-    console.log("SW claiming");
-    self.clients.claim();
-    console.log("SW activated");
-
+    console.log("SW activating ..")
     event.waitUntil(
         caches.keys()
         .then(function (keys){
@@ -46,6 +44,9 @@ self.addEventListener("activate", function (event){
             }));
         })
         .then((r) => {
+            console.log("SW claiming");
+            self.clients.claim();
+            console.log("SW activated => version ", version);
             console.log(r);
             return r;
         })
@@ -61,9 +62,51 @@ toolbox.router.get("/hoodie/client.js", toolbox.cacheFirst, {
     }
 });
 
-toolbox.router.get("/hoodie/*", toolbox.networkOnly);
+//toolbox.router.get("/hoodie/*", toolbox.networkOnly);//include a timer to allow quick XHR request termination when offline?
 
-toolbox.router.get("/*", toolbox.cacheFirst, {
+toolbox.router.get("/", toolbox.cacheFirst, {
+    cache: {
+        name: version,
+        maxAgeSeconds: 60 * 60 * 24 * 365
+    }
+});
+
+toolbox.router.get("/index.html", toolbox.cacheFirst, {
+    cache: {
+        name: version,
+        maxAgeSeconds: 60 * 60 * 24 * 365
+    }
+});
+
+toolbox.router.get("/history.html", toolbox.cacheFirst, {
+    cache: {
+        name: version,
+        maxAgeSeconds: 60 * 60 * 24 * 365
+    }
+});
+
+toolbox.router.get("/manifest.json", toolbox.cacheFirst, {
+    cache: {
+        name: version,
+        maxAgeSeconds: 60 * 60 * 24 * 365
+    }
+});
+
+toolbox.router.get("/resources/*", toolbox.cacheFirst, {
+    cache: {
+        name: version,
+        maxAgeSeconds: 60 * 60 * 24 * 365
+    }
+});
+
+toolbox.router.get("/css/*", toolbox.cacheFirst, {
+    cache: {
+        name: version,
+        maxAgeSeconds: 60 * 60 * 24 * 365
+    }
+});
+
+toolbox.router.get("/js/*", toolbox.cacheFirst, {
     cache: {
         name: version,
         maxAgeSeconds: 60 * 60 * 24 * 365
