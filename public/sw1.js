1a861bf7ff4a5cb424f569ba30b6fbb165daa484
Peter Mbanugo <chuksgerrard@yahoo.com>
2018-01-22 22:09:33 +0100

    keep old sw script

---
+++ public/sw1.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "cache-v1";
+
+const assetToCache = [
+  "/index.html",
+  "/",
+  "/history.html",
+  "/manifest.json",
+  "/resources/mdl/material.indigo-pink.min.css",
+  "/resources/mdl/material.min.js",
+  "/resources/mdl/MaterialIcons-Regular.woff2",
+  "/resources/mdl/material-icons.css",
+  "/css/style.css",
+  "/resources/dialog-polyfill/dialog-polyfill.js",
+  "/resources/dialog-polyfill/dialog-polyfill.css",
+  "/resources/system.js",
+  "/js/transpiled/index.js",
+  "/js/transpiled/history.js",
+  "/js/transpiled/shared.js",
+  "/hoodie/client.js"
+];
+
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(assetToCache);
+      })
+      .catch(console.error)
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response; //return the matching entry is found
+      }
+      return fetch(event.request);
+    })
+  );
+});
